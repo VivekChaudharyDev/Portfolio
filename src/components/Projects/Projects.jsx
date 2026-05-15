6220b398aa7aa78bb8c7e9df666c0d2c2c50bc85
Vivek Chaudhary <vivekchaudharycs@gmail.com>
2026-05-15 20:24:11 +0530

    fix clip path css

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,7 +3,7 @@ import { projects } from '../../constants';
 
 const Projects = () => {
   return (
-    <section id="projects" className="py-24 pb-24 px-4 md:px-[7vw] lg:px-[15vw] font-sans dark:bg-skills-gradient-dark bg-skills-gradient-light overflow-x-hidden clip-path-custom-reverse">
+    <section id="projects" className="py-24 pb-24 px-4 md:px-[7vw] lg:px-[15vw] font-sans dark:bg-skills-gradient-dark bg-skills-gradient-light overflow-x-hidden clip-path-custom">
       <h1 className="text-4xl dark:text-dark-text text-light-text">Projects Section</h1>
     </section>
   )
